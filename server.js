8ec90ecaa6cabe289cb873d8cdcecf3bfc4d9259
Prajna <prajna.yogish4@gmail.com>
2024-09-07 23:43:49 -0400

    different types of response,accesing query parameter,status

--- server.js
+++ server.js
@@ -34,10 +34,32 @@ let posts = [
         title:"Post three"
     }
 ]
-
+//get all posts
 app.get('/api/posts',(req,res)=>{
-res.json(posts)
+    console.log(req.query)
+    const limit = parseInt(req.query.limit);
+    if(!isNaN(limit)&& limit>0){
+res.status(200).json(posts.slice(0,limit));
+    }else{
+        res.status(200).json(posts)
+    }
+
 })
+
+//get single post
+app.get('/api/posts/:id',(req,res)=>{
+   const id = parseInt(req.params.id);
+  const post = posts.find((post)=> post.id === id) ;
+  if(post){
+    res.status(200).json(post)
+  }else{
+    // res.status(404).send(`Post with ${id} not found`)
+res.status(404).json({msg: `Post with ${id} not found`})  
+}
+// res.status(200).json(posts.filter((post)=> post.id ===id));
+
+})
+
 
 app.listen(PORT,()=>{
     console.log(`server listening at ${PORT}`);
